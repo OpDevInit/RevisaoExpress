72a6845393079639aee0d5a331c7a3b00f5d29ae
Jholl <jholzinhooh567@gmail.com>
2022-03-28 19:58:48 -0300

    npm iniciado

---
+++ uteis.js
@@ -0,0 +1,14 @@
+module.export = {
+
+    ehPrimo: function(valor){
+        for(let i=2; i< valor/2;i++){
+            if(valor%i == 0){
+                return  false;
+            }
+           
+
+        }
+        return true;
+    }
+
+}
