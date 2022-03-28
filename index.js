040b2904c0f6fee66a23be0a6bc487b6c4129746
Jholl <jholzinhooh567@gmail.com>
2022-03-28 20:19:59 -0300

    Express Adicionado

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 let util = require("./uteis");
-console.log("eai meu bom!!");
+console.log("iniciando meu  Servidor!!");
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req,res)=>{
+    res.send("To de Volta!!");
+})
+
+app.listen(port, () =>{
+    console.log(`Porta em execucao...${port}`);
+})
 
 
 
