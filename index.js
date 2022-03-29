77840236221f1d091cd734753ecda1a76ad15ac1
Jholl <jholzinhooh567@gmail.com>
2022-03-28 21:14:03 -0300

    1º Teste get na URL

--- index.js
+++ index.js
@@ -5,11 +5,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res)=>{
-    res.send("To de Volta!!");
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+})
+//pega os dados listados na url
+app.get('/teste/algo/:nome/:idade', (req, res) => {
+  
+  console.log("Alguem acessou a rota /teste/algo");
+  console.log(req.params);
+  
+  res.send(`
+    <div style="width: 300px; border: 1px solid black; padding: 4px;">
+        <p style="font-weight: 800;">Você acessou a rota</p> 
+        /teste/algo
+        <p>nome: ${req.params.nome}</p>
+        <p>idade: ${req.params.idade}</p>
+    </div>
+  `)
+
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Porta em execucao...${port}`);
 })
 
